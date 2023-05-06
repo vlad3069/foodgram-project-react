905d3c69abaa6f123a3996d16aabb6537484d5b7
vlad3069 <vlad9603@gmail.com>
2023-05-06 12:31:35 +0300

    db

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='123456789'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
         'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default=5432)
     }
