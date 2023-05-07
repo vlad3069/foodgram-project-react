8ded08c483f3358a2fbc7b7e90f9d8064b7be564
vlad3069 <vlad9603@gmail.com>
2023-05-07 15:44:38 +0300

    sett

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -68,8 +68,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='foodgram_db'),
-        'USER': os.getenv('POSTGRES_USER', default='foodgram_db'),
+        'NAME': os.getenv('DB_NAME', default='foodgram_user'),
+        'USER': os.getenv('POSTGRES_USER', default='foodgram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='123456789'),
         'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default=5432)
