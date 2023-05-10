282ccedb9500da2c53124de57ba3285f7c578ebe
vlad3069 <vlad9603@gmail.com>
2023-05-10 16:15:40 +0300

    urls

--- backend/backend/api/urls.py
+++ backend/backend/api/urls.py
@@ -10,14 +10,35 @@ auth_urls_v1 = [
     path(r'token/logout/', TokenDestroyView.as_view(), name='logout'),
 ]
 
+users_urls_v1 = [
+    path(r'', UserViewSet, 'users'),
+    path(r'<int:id>/', UserViewSet, 'user-detail'),
+    path(r'me/', UserViewSet.as_view(), name='me-detail'),
+    path(
+        r'set_password/',
+        UserViewSet.as_view(),
+        name='set-password',
+    ),
+    path(
+        r'subscriptions/',
+        UserViewSet.as_view(),
+        name='subscriptions',
+    ),
+    path(
+        r'<int:author_id>/subscribe/',
+        UserViewSet.as_view(),
+        name='subscribe',
+    ),
+]
 
 router_v1 = DefaultRouter()
 router_v1.register(r'tags', TagViewSet, basename='tags')
 router_v1.register(r'ingredients', IngredientViewSet, basename='ingredients')
 router_v1.register(r'recipes', ReciepeViewSet, basename='recipes')
-router_v1.register(r'users', UserViewSet, 'users')
+
 
 urlpatterns = [
     path(r'auth/', include(auth_urls_v1)),
+    path(r'users/', include(users_urls_v1)),
     path(r'', include(router_v1.urls)),
 ]
