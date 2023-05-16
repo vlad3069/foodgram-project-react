7c5bb57874c90c3f88957cb3249cd4a30c5f6e69
vlad3069 <vlad9603@gmail.com>
2023-05-16 17:44:16 +0300

    filt

--- backend/backend/api/filters.py
+++ backend/backend/api/filters.py
@@ -1,10 +1,12 @@
+from django.db.models.query_utils import Q
 from django_filters.rest_framework import FilterSet, filters
 
-from recipes.models import Recipe
+from recipes.models import FavoriteRecipe, Recipe, ShoppingCartRecipe
 from tags.models import Tag
 
 
 class FilterRecipe(FilterSet):
+    queryset = Recipe.objects.all()
     tags = filters.ModelMultipleChoiceFilter(field_name='tags__slug',
                                              to_field_name='slug',
                                              queryset=Tag.objects.all())
@@ -17,14 +19,34 @@ class FilterRecipe(FilterSet):
         model = Recipe
         fields = ('tags', 'author',)
 
-    def is_favorited_filter(self, queryset, name, value):
+    def is_favorited_filter(self, queryset):
         user = self.request.user
-        if value and user.is_authenticated:
-            return queryset.filter(favorite_recipe__user=user)
+        is_favorited = self.request.query_params.get('is_favorited')
+        if is_favorited:
+            recipes_id = (
+                FavoriteRecipe.objects.filter(user=user).values('recipe__id')
+                if user.is_authenticated
+                else []
+            )
+            condition = Q(id__in=recipes_id)
+            queryset = queryset.filter(
+                condition if is_favorited == '1' else ~condition
+            ).all()
         return queryset
 
-    def is_in_shopping_cart_filter(self, queryset, name, value):
+    def is_in_shopping_cart_filter(self, queryset):
         user = self.request.user
-        if value and user.is_authenticated:
-            return queryset.filter(shopping_recipe__user=user)
+        is_in_shopping_cart = self.request.query_params.get(
+            'is_in_shopping_cart')
+        if is_in_shopping_cart:
+            recipes_id = (
+                ShoppingCartRecipe.objects.filter(user=user).values(
+                    'recipe__id')
+                if user.is_authenticated
+                else []
+            )
+            condition = Q(id__in=recipes_id)
+            queryset = queryset.filter(
+                condition if is_in_shopping_cart == '1' else ~condition
+            ).all()
         return queryset
